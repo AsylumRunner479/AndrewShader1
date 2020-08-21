14ee92da4fa0a8c6a6772360615b54a748e9a415
AsylumRunner479 <54158974+AsylumRunner479@users.noreply.github.com>
2020-08-21 10:00:22 +1000

    add action map

---
+++ AndrewShader/Assets/Scripts/CubePlayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class CubePlayer : MonoBehaviour
+{
+    PlayerInput playerInput;
+    Mover mover;
+    private void Awake()
+    {
+        playerInput = GetComponent<PlayerInput>();
+        var movers = FindObjectsOfType<Mover>();
+        var index = playerInput.playerIndex;
+        mover = movers.FirstOrDefault(m => m.GetPlayerIndex() == index);
+    }
+    public void OnMove(InputValue value)
+    {
+        if (mover != null)
+            mover.SetInputVector(value.Get<Vector2>());
+    }
+}
